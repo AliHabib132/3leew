34a6aad1225273fd087e862aa3c0ed1a95580620
Ali Habib <ali.habib132@gmail.com>
2020-12-10 20:18:24 +0300

    init

---
+++ WPTS/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPTS.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage ="This filed is required")]
+        [DisplayName("Emp. No.")]
+        [Column(TypeName = "int")]
+        public int EmployeeNumber { get; set; }
+
+        [DisplayName("Full Name")]
+        [Column(TypeName ="varchar(250)")]
+        public string FullName { get; set; }
+
+        [DisplayName("Employee Trade")]
+        [Column(TypeName ="varchar(100)")]
+        public string Position { get; set; }
+        
+    }
+}
---
+++ WPTS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WPTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
